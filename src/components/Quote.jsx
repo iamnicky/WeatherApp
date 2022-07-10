72ce8bd98a42ad402387a457a9fb97cc99d088f0
iamnicky <2020ugee067@nitjsr.ac.in>
2022-07-10 23:44:34 +0530

    alert msg changed

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function Quote() {
     const clicked = () => {
-        alert("Will change the quote on clicking this button!")
+        alert("new quote arrived!")
     }
     return (
         <div className='bg-violet-600 h-[100vh] w-[600px]'>
             <h1 className='text-6xl mt-32 ml-16 font- text-black'>My Quote will appear here</h1>
             <p className='mt-12 ml-16 text-gray-900'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis nemo commodi sequi nostrum ipsam beatae magnam sit, neque laborum, distinctio aperiam. Aut laborum tenetur veritatis accusamus! Libero, consequuntur quisquam.</p>
-            <button onClick={clicked} className='bg-black text-white ml-16 mt-32 px-8 py-4 rounded-2xl text-lg'>Next Quote</button>
+            <button onClick={clicked} className='hover:motion-safe:animate-bounce bg-black text-white ml-16 mt-32 px-8 py-4 rounded-2xl text-lg'>Next Quote</button>
 
         </div>
     )
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -9,31 +9,32 @@ function Weather(props) {
             <input type='text' placeholder="Search City" className='px-8 mt-16 bg-slate-400 py-2 text-white text-lg border-none outline-none rounded-xl' />
 
             {/* <lottie-player className='' src="https://assets5.lottiefiles.com/packages/lf20_cHA3rG.json" background="transparent" speed="1" ></lottie-player> */}
+            <img src=''className='mt-12'/>
+
             <article className='mt-8 py-8 px-12 rounded-lg bg-white shadow-xl flex flex-col items-center justify-center'>
                <h1 className='text-7xl font-bold'>30 °C</h1>
                <p className='text-gray-900 font-semibold text-3xl mt-8'>Sunny Day</p>
             </article>
 
-            <div className='h-1 w-full mt-24 bg-gray-700'></div>
+            <div className='h-[1px] w-full mt-24 bg-gray-700'></div>
+            
             <article className='h-24 w-full mt-4 flex justify-evenly'>
 
                <div className='px-1 flex flex-col justify-center items-center'>
                 <h1 className='text-lg text-gray-600'>Precipitation</h1>
-                <h1 className='text-md text-gray-500'>38</h1>
-                
+                <h1 className='text-md text-gray-500'>38</h1>                
                </div>
 
                
                <div className='px-1 flex flex-col justify-center items-center'>
                 <h1 className='text-lg text-gray-600'>Humidity</h1>
-                <h1 className='text-md text-gray-500'>32%</h1>
-                
+                <h1 className='text-md text-gray-500'>32%</h1>                
                </div>
+
                
                <div className='px-1 flex flex-col justify-center items-center'>
                 <h1 className='text-lg text-gray-600'>Wind</h1>
-                <h1 className='text-md text-gray-500'>2.4 km/s</h1>
-                
+                <h1 className='text-md text-gray-500'>2.4 km/s</h1>                
                </div>
             </article>
             
