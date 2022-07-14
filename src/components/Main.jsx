f261cffa004a18a310c0a5e0c924fd0f3feaade9
iamnicky <2020ugee067@nitjsr.ac.in>
2022-07-15 00:19:41 +0530

    greeting added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import Quote from './Quote'
 import Weather from './Weather'
+import { useState } from 'react';
+import moment from 'moment';
 
 
 function Main(props) {
   const { handleToggleDarkMode,weather,setCity,fetchweather} = props;
 
+
+
+  var date= new Date();
+  var hr = date.getHours();
+  
+  
+
+ 
+
   return (
 
     <div className=' flex-wrap '>
 
-<div className='flex justify-end p-2 mx-4  '>
-
+<div className='flex justify-between  p-2 mx-4  '>
+ <h1 className='font-mono  font-bold text-5xl text-white capitalize'>{hr<12?<h1>good morning☀️</h1>:(hr<16?<h1>good afternoon🌤️</h1>:(hr<18?<h1>good evening⛅</h1>:(hr<24?<h1>good night🌙</h1>:<h1>time not fetched error</h1>)))}</h1>
 <button className=' text-green-800 border-4 rounded shadow-3xl border-gray-300 bg-yellow-300 flex jutify-center' onClick={() => handleToggleDarkMode((previousDarkMode) => !previousDarkMode)}><img src="https://img.icons8.com/color/48/000000/back-sorting.png" /></button>
 
 </div>
